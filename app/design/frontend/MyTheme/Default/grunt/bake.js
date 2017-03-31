aeced4206919024a0db1c1e71d213c1cd80d6725
Matt Bailey <matt@gpmd.co.uk>
2017-03-31 13:43:37 +0100

    Updates

--- app/design/frontend/MyTheme/Default/grunt/bake.js
+++ app/design/frontend/MyTheme/Default/grunt/bake.js
@@ -7,6 +7,13 @@ module.exports = function (grunt, data) {
           toUpper: function(str) {
             return String(str).toUpperCase();
           },
+          toTitleCase: function(str) {
+            str = str.toLowerCase().split(' ');
+            for (var i = 0; i < str.length; i++) {
+              str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+            }
+            return str.join(' ');
+          },
           deHyphenate: function(str) {
             return String(str).replace(/-/g, ' ');
           }
